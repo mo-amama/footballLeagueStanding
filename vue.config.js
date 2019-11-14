ae4151dbabac905522b17d650cc8c1c88cb960a6
Moses Amama <amaofficial07@gmail.com>
2019-11-14 19:33:01 +0100

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pwa: {
+    name: 'football league standing',
+    workboxPluginMode: 'InjectManifest',
+    themeColor: '#4A90E2',
+    workboxOptions: {
+      swSrc: './src/sw.js',
+      swDest: 'service-worker.js',
+    },
+  }
+}
